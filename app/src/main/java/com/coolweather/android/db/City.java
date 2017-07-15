3b5d1f8ffe807603682626c0b4d4717f1b6247b5
Xdeam <wangcilv@gmail.com>
2017-07-15 12:49:20 +0800

    just code

--- app/src/main/java/com/coolweather/android/db/City.java
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -1,8 +1,48 @@
 package com.coolweather.android.db;
 
+import org.litepal.crud.DataSupport;
+
 /**
  * Created by kwinter on 2017/7/14.
  */
 
-public class City {
+public class City extends DataSupport{
+    private int id;
+    private String cityName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    private int cityCode;
+    private int provinceId;
+
 }
--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -1,8 +1,40 @@
 package com.coolweather.android.db;
 
+import org.litepal.crud.DataSupport;
+
 /**
  * Created by kwinter on 2017/7/14.
  */
 
-public class Province {
+public class Province extends DataSupport {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+
+
 }
